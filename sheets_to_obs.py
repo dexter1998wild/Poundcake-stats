c7f7a987ddf10f04c912ad62fdb52d92caac7f6e
dexter1998wild <38758158+dexter1998wild@users.noreply.github.com>
2023-05-23 13:20:43 -0400

    Create sheets_to_obs.py
    Sheets_to_obs.py: Takes data from google stat sheet and outputs a formatted text file with required information.
    
    Library Requirements:
    1. gspread
    2. pandas
    
    File Requirements:
    1. Google  Cloud API service account key as a json file
    2. output text file path

---
+++ sheets_to_obs.py
@@ -0,0 +1,53 @@
+"""
+Created on Tue May 23 12:21:35 2023
+
+@author: dexter
+"""
+import gspread as gs
+import pandas as pd
+
+gc = gs.service_account(filename="<google API service account key json file>")
+sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/1vCcApZK2EVmAmpxkFGzFs9Ys76zHXCa61U5gkSzZNWs/edit#gid=0')
+sheet=sh.worksheet('Sheet1').get_all_values()
+df = pd.DataFrame(sheet[1:], columns=sheet[0])
+#print(df.head())
+
+igt_times1=list(df['Time'])
+dates_played=df['Date']
+dates=[]
+run_number=df['Run Counter']
+max_run_number=0
+
+for i in range(len(dates_played)):
+  if dates_played[i]!='':
+    dates.append([i,dates_played[i]])
+
+runs_per_day=[i[0] for i in dates]
+date = [i[1] for i in dates]
+
+for i in range(len(runs_per_day)-2,len(runs_per_day)-1):
+  times=[]
+  
+  for j in range(runs_per_day[i],runs_per_day[i+1]):
+    #print(igt_times1[j])
+    
+    
+    if igt_times1[j]=='':
+      continue
+    if j==0:
+      continue
+    else:
+      times.append(igt_times1[j])
+      max_run_number=run_number[j]
+print(times)
+print(max_run_number)
+with open('<Text file to write data into>', 'w') as f:
+    content='Sub 20s: '+str(max_run_number)+'/1000 \nToday: \n'
+    
+    for i in range(len(times)):
+        content+=str(times[i])
+        content+=', '
+        if i%2==1:
+            content+='\n'
+    f.write(content)
+    
